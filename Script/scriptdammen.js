d383b6afe240ac7c4a03edbf9ed7540de5ec4b3f
Roy de Graaf <roydegraaf8@hotmail.com>
2019-02-14 09:47:53 +0100

    Nummers goed geplaatst

--- Script/scriptdammen.js
+++ Script/scriptdammen.js
@@ -1,6 +1,7 @@
 var steen = document.getElementsByClassName("steen");
+var nummer = document.getElementsByClassName("number");
 for (i=0; i < steen.length; i++){
   steen[i].addEventListener("click",function() {
-  console.log("kek");
+  //zet hier het script om de stenen te verlpaatsen
   })
 }
