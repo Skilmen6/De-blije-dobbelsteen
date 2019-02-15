1f469d34cea02e54bd7232bafe933208ac846d00
Roy de Graaf <roydegraaf8@hotmail.com>
2019-02-15 10:47:26 +0100

    Hele bord opnieuw gemaakt in javascript

--- Script/scriptdammen.js
+++ Script/scriptdammen.js
@@ -1,7 +1,75 @@
-var steen = document.getElementsByClassName("steen");
-var nummer = document.getElementsByClassName("number");
-for (i=0; i < steen.length; i++){
-  steen[i].addEventListener("click",function() {
-  //zet hier het script om de stenen te verlpaatsen
-  })
+var board = [
+  ["c", "Z", "c", "Z", "c", "Z", "c", "Z", "c", "Z"],
+  ["Z", "c", "Z", "c", "Z", "c", "Z", "c", "Z", "c"],
+  ["c", "Z", "c", "Z", "c", "Z", "c", "Z", "c", "Z"],
+  ["Z", "c", "Z", "c", "Z", "c", "Z", "c", "Z", "c"],
+  ["c", "b", "c", "b", "c", "b", "c", "b", "c", "b"],
+  ["b", "c", "b", "c", "b", "c", "b", "c", "b", "c"],
+  ["c", "W", "c", "W", "c", "W", "c", "W", "c", "W"],
+  ["W", "c", "W", "c", "W", "c", "W", "c", "W", "c"],
+  ["c", "W", "c", "W", "c", "W", "c", "W", "c", "W"],
+  ["W", "c", "W", "c", "W", "c", "W", "c", "W", "c"]
+]
+
+createBoard();
+
+function createBoard(){
+  var container = document.getElementById("game-container");
+
+  for (r=0; r < board.length; r++){
+    var row = createRow();
+    container.appendChild(row);
+
+    for (i=0; i < board[r].length; i++){
+
+      switch(board[r][i]){
+        case "c":;
+          row.appendChild(createEmpty());
+          break;
+
+        case "Z":
+          var empty = row.appendChild(createEmpty());
+          empty.appendChild(createBlack());
+          break;
+
+        case "b":
+          row.appendChild(createEmpty());
+          break;
+
+        case "W":
+          var empty = row.appendChild(createEmpty());
+          empty.appendChild(createWhite());
+          break;
+      }
+    }
+  }
+}
+
+function createBlack(){
+  var black = document.createElement("img");
+  black.setAttribute("src", "Img/Zwartesteen.png");
+  return black;
+}
+
+function createWhite(){
+  var white = document.createElement("img");
+  white.setAttribute("src", "Img/wittesteen.png");
+  return white;
+}
+
+function createRow(){
+  var row = document.createElement("div");
+  row.setAttribute("class", "row-container");
+  return row;
+}
+
+function createEmpty(){
+  var empty = document.createElement("div");
+  empty.setAttribute("class", "spelvak");
+  return empty;
+}
+
+function createSpace(){
+  var space = document.createElement("br");
+  return space;
 }
