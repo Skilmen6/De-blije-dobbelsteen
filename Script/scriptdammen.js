645b61fe40ff49a04f6cc4256e0ccc6914a25b53
Roy de Graaf <Roydegraaf8@hotmail.com>
2019-02-17 14:37:28 +0100

    Witte stenen kunnen nu bewegen

--- Script/scriptdammen.js
+++ Script/scriptdammen.js
@@ -24,36 +24,54 @@ function createBoard(){
 
       switch(board[r][i]){
         case "c":;
-          row.appendChild(createEmpty());
+          var empty = createEmptyW();
+          empty.setAttribute("id", r + "." + i);
+          row.appendChild(empty);
           break;
 
         case "Z":
-          var empty = row.appendChild(createEmpty());
-          empty.appendChild(createBlack());
+          var empty = row.appendChild(createEmptyW());
+          var black = createBlack();
+          black.setAttribute("id", r + "." + i);
+          empty.appendChild(black);
           break;
 
         case "b":
-          row.appendChild(createEmpty());
+          var empty = createEmptyZ();
+          empty.setAttribute("id", r + "." + i);
+          row.appendChild(empty);
           break;
 
         case "W":
-          var empty = row.appendChild(createEmpty());
-          empty.appendChild(createWhite());
+          var empty = row.appendChild(createEmptyW());
+          var white = createWhite();
+          white.setAttribute("id", r + "." + i);
+          empty.appendChild(white);
           break;
       }
     }
   }
 }
 
+
+
 function createBlack(){
   var black = document.createElement("img");
   black.setAttribute("src", "Img/Zwartesteen.png");
+
+  black.addEventListener("click", function(){
+    movePiece(this);
+  });
   return black;
 }
 
 function createWhite(){
   var white = document.createElement("img");
   white.setAttribute("src", "Img/wittesteen.png");
+
+  white.addEventListener("click", function(){
+    movePiece(this);
+  });
   return white;
 }
 
@@ -63,13 +81,58 @@ function createRow(){
   return row;
 }
 
-function createEmpty(){
+function createEmptyW(){
   var empty = document.createElement("div");
   empty.setAttribute("class", "spelvak");
   return empty;
 }
 
-function createSpace(){
-  var space = document.createElement("br");
-  return space;
+function createEmptyZ(){
+  var empty = document.createElement("div");
+  empty.setAttribute("class", "spelvak");
+
+  empty.addEventListener("click", function(){
+    movePiece(this);
+  });
+  return empty;
+}
+
+function clearChildren(p){
+  while(p.firstChild) {
+    p.removeChild(p.firstChild)
+  }
+}
+
+from = null;
+
+function movePiece(elem){
+  console.log(elem);
+  if (elem.src == "http://localhost/Github/De-blije-dobbelsteen/Img/wittesteen.png"){
+    from = elem;
+  }
+  else if (from){
+    var container = document.getElementById("game-container");
+    var posfrom = from.id.split(".");
+    var posto = elem.id.split(".");
+
+    var fromr = parseInt(posfrom[0]);
+    var fromi = parseInt(posfrom[1]);
+
+    var tor = parseInt(posto[0]);
+    var toi = parseInt(posto[1]);
+
+    console.log(tor);
+    console.log(board[tor][toi]);
+
+    board[tor].splice(toi, 1, "W");
+
+
+
+    board[fromr].splice(fromi, 1, "b");
+
+
+    from = null;
+    clearChildren(container);
+    createBoard();
+  }
 }
