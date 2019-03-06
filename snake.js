030125a6f0139dc489a6ea4241e763a193910f15
Dante <532488@edu.rocmn.nl>
2019-03-06 15:21:31 +0100

    highscore toegevoegd

--- snake.js
+++ snake.js
@@ -1,125 +1,241 @@
-var canvas = document.getElementById('game');
-var context = canvas.getContext('2d');
-var grid = 16;
-var count = 0;
-var score = 0;
-var snake = {
-    x: 160,
-    y: 160,
 
-    //snake snelheid. Elke grid lengte in x en y kant
-    dx: grid,
-    dy: 0,
+var
 
-    //kijkt bij fouten van snake
-    cells: [],
+    COLS = 26,
+    ROWS = 26,
+    EMPTY = 0,
+    SNAKE = 1,
+    FRUIT = 2,
+    LEFT = 0,
+    UP = 1,
+    RIGHT = 2,
+    DOWN = 3,
+    KEY_LEFT = 37,
+    KEY_UP = 38,
+    KEY_RIGHT = 39,
+    KEY_DOWN = 40,
 
-    // lengte van de slang na het eten van een appel
-    maxCells: 4
-};
-var apple = {
-    x: 320,
-    y: 320
-};
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-// game loop
-function loop() {
+    canvas,
+    ctx,
+    keystate,
+    frames,
+    score,
+    highscore = 0;
 
-    requestAnimationFrame(loop);
-    // Snelheid van de slang
-    if (++count < 6) {
-        return;
-    }
+grid = {
+    width: null,
+    height: null,
+    _grid: null,
 
-    count = 0;
-    context.clearRect(0,0,canvas.width,canvas.height);
-    // snelheid van de slang velocity
-    snake.x += snake.dx;
-    snake.y += snake.dy;
-    // horzontally position van hoek van scherm
-    if (snake.x < 0) {
-        snake.x = canvas.width - grid;
-    }
-    else if (snake.x >= canvas.width) {
-        snake.x = 0;
-    }
-
-    // Verticaal position van hoek van scherm
-    if (snake.y < 0) {
-        snake.y = canvas.height - grid;
-    }
-    else if (snake.y >= canvas.height) {
-        snake.y = 0;
-    }
-    //zien waar de slang is geweest
-    snake.cells.unshift({x: snake.x, y: snake.y});
-    if (snake.cells.length > snake.maxCells) {
-        snake.cells.pop();
-    }
-    // teken de appel
-    context.fillStyle = 'red';
-    context.fillRect(apple.x, apple.y, grid-1, grid-1);
-    // tekent de slang 1 cell per keer
-    context.fillStyle = 'lime';
-    snake.cells.forEach(function(cell, index) {
-
-
-        // tekent de 1px grid waardoor je kan zien hoelang de grid is.
-        context.fillRect(cell.x, cell.y, grid-1, grid-1);
-        // slang die de appel eet
-        if (cell.x === apple.x && cell.y === apple.y) {
-            snake.maxCells++;
-            score++;
-            // canvas is 400x400 elke grid is 25x25.
-            apple.x = getRandomInt(0, 25) * grid;
-            apple.y = getRandomInt(0, 25) * grid;
-        }
-
-        // kijkt of de cells niet door elkaar heen gaan.
-        for (var i = index + 1; i < snake.cells.length; i++) {
-
-            // als de slang zich zelf raakt dan begin je opnieuw.
-            if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-                snake.x = 160;
-                snake.y = 160;
-                snake.cells = [];
-                snake.maxCells = 4;
-                snake.dx = grid;
-                snake.dy = 0;
-                apple.x = getRandomInt(0, 25) * grid;
-                apple.y = getRandomInt(0, 25) * grid;
+    init: function (d, c, r) {
+        this.width = c;
+        this.height = r;
+        this._grid = [];
+        for (var x = 0; x < c; x++) {
+            this._grid.push([]);
+            for (var y = 0; y < r; y++) {
+                this._grid[x].push(d);
             }
         }
-    });
+    },
+
+    set: function (val, x, y) {
+        this._grid[x][y] = val;
+    },
+
+    get: function (x, y) {
+        return this._grid[x][y];
+    }
 }
-// zorgt ervoor dat de slang luistert op de toetsen
-document.addEventListener('keydown', function(e) {
 
-    // naar links
-    if (e.which === 37 && snake.dx === 0) {
-        snake.dx = -grid;
-        snake.dy = 0;
+snake = {
+    direction: null,
+    last: null,
+    _queue: null,
+
+    init: function (d, x, y) {
+        this.direction = d;
+        this._queue = [];
+        this.insert(x, y);
+    },
+
+    insert: function (x, y) {
+
+        this._queue.unshift({x: x, y: y});
+        this.last = this._queue[0];
+    },
+
+    remove: function () {
+        // pop returns the last element of an array
+        return this._queue.pop();
     }
-    // naar boven
-    else if (e.which === 38 && snake.dy === 0) {
-        snake.dy = -grid;
-        snake.dx = 0;
+};
+
+function setFood() {
+    var empty = [];
+    for (var x = 0; x < grid.width; x++) {
+        for (var y = 0; y < grid.height; y++) {
+            if (grid.get(x, y) === EMPTY) {
+                empty.push({x: x, y: y});
+            }
+        }
     }
-    // naar rechts
-    else if (e.which === 39 && snake.dx === 0) {
-        snake.dx = grid;
-        snake.dy = 0;
+    // chooses a random cell
+    var randpos = empty[Math.round(Math.random() * (empty.length - 1))];
+    grid.set(FRUIT, randpos.x, randpos.y);
+}
+
+/**
+ * Starts the game
+ */
+function main() {
+
+    canvas = document.createElement("canvas");
+    canvas.width = COLS*20;
+    canvas.height = ROWS*20;
+    ctx = canvas.getContext("2d");
+    // add the canvas element to the body of the document
+    document.body.appendChild(canvas);
+    ctx.font = "12px Helvetica";
+    frames = 0;
+    keystate = {};
+    document.addEventListener("keydown", function(evt) {
+        keystate[evt.keyCode] = true;
+    });
+    document.addEventListener("keyup", function(evt) {
+        delete keystate[evt.keyCode];
+    });
+
+    init();
+    loop();
+}
+/**
+ * Resets
+ */
+function init() {
+    score = 0;
+    grid.init(EMPTY, COLS, ROWS);
+    var sp = {x:Math.floor(COLS/2), y:ROWS-1};
+    snake.init(UP, sp.x, sp.y);
+    grid.set(SNAKE, sp.x, sp.y);
+    setFood();
+}
+/**
+ * The game loop function
+ */
+function loop() {
+    update();
+    draw();
+
+    window.requestAnimationFrame(loop, canvas);
+}
+
+function update() {
+    frames++;
+
+    if (keystate[KEY_LEFT] && snake.direction !== RIGHT) {
+        snake.direction = LEFT;
     }
-    // naar beneden
-    else if (e.which === 40 && snake.dy === 0) {
-        snake.dy = grid;
-        snake.dx = 0;
+    if (keystate[KEY_UP] && snake.direction !== DOWN) {
+        snake.direction = UP;
     }
-    ctx.fillStyle = "#000";
+    if (keystate[KEY_RIGHT] && snake.direction !== LEFT) {
+        snake.direction = RIGHT;
+    }
+    if (keystate[KEY_DOWN] && snake.direction !== UP) {
+        snake.direction = DOWN;
+    }
+    if (frames%5 === 0) {
+        // pop the last element from the snake
+        // head
+        var nx = snake.last.x;
+        var ny = snake.last.y;
+        // updates the position
+        switch (snake.direction) {
+            case LEFT:
+                nx--;
+                break;
+            case UP:
+                ny--;
+                break;
+            case RIGHT:
+                nx++;
+                break;
+            case DOWN:
+                ny++;
+                break;
+        }
+        if (0 > nx || nx > grid.width-1  ||
+            0 > ny || ny > grid.height-1 ||
+            grid.get(nx, ny) === SNAKE
+        ) {
+            if(score > highscore) {
+                highscore = score;
+            }
+            return init();
+        }
+        if (grid.get(nx, ny) === FRUIT) {
+            // New position for Food
+            score++;
+            setFood();
+        } else {
+
+            var tail = snake.remove();
+            grid.set(EMPTY, tail.x, tail.y);
+        }
+
+        grid.set(SNAKE, nx, ny);
+        snake.insert(nx, ny);
+    }
+}
+
+function draw() {
+
+    var tw = canvas.width/grid.width;
+    var th = canvas.height/grid.height;
+
+    for (var x=0; x < grid.width; x++) {
+        for (var y=0; y < grid.height; y++) {
+
+            switch (grid.get(x, y)) {
+                case EMPTY:
+                    ctx.fillStyle = "#3c702c";
+                    break;
+                case SNAKE:
+                    ctx.fillStyle = "#1fa327";
+                    break;
+                case FRUIT:
+                    ctx.fillStyle = "#f00";
+                    break;
+            }
+            ctx.fillRect(x*tw, y*th, tw, th);
+        }
+    }
+    // message to the canvas
+    ctx.fillStyle = "#ffffff";
     ctx.fillText("SCORE: " + score, 10, canvas.height-10);
-});
+    ctx.fillText("HIGH SCORE: " + highscore, canvas.width - 100, canvas.height-10);
+}
+// start and run the game
+main()
+var record = 100;
 
-//start de game.
-requestAnimationFrame(loop);
+
+
+function getCookie(c_name)
+{
+    if (document.cookie.length>0)
+    {
+        c_start=document.cookie.indexOf(c_name + "=");
+        if (c_start!=-1)
+        {
+            c_start=c_start + c_name.length+1 ;
+            c_end=document.cookie.indexOf(";",c_start);
+            if (c_end==-1) c_end=document.cookie.length
+            return unescape(document.cookie.substring(c_start,c_end));
+        }
+    }
+    return "";
+}
+
+
---
+++ snake.php
@@ -0,0 +1,8 @@
+<?php
+// dit staat nog niet vast aan snake.
+echo ("Submit je highscore!");
+print("Naam: "); print $_POST['Naam']; print "<br>";
+print("Highscore: "); print $_POST['Highscore']; print "<br>";
+$zin = "je naam is:" . 'naam' . "je hebt" . 'Highscore' . "punten gehaald";
+print $zin;
+?>
